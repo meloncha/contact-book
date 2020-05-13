74af5f8f45c588a5feee049616cad5b8cc1da393
melon <meloncha0205@gmail.com>
2020-05-13 20:58:51 +0900

    22

--- contact-book/index.js
+++ contact-book/index.js
@@ -42,7 +42,7 @@ let port = 3000;
 app.listen(port, () => {
     console.log('server on! http://localhost:' + port);
 });
-*/
+
 
 //index.js 2
 let express = require('express');
@@ -146,4 +146,119 @@ app.post('/contacts', (req, res)=>{
 let port = 3000;
 app.listen(port, () => {
     console.log('Server on! http://localhost:' + port);
+});
+*/
+
+// index.js 3
+let express = require('express');
+let mongoose = require('mongoose');
+let bodyparser = require('body-parser');
+let methodOverride = require('method-override');
+//method-override module을 methodOverride변수에 담습니다.
+let app = express();
+
+//DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.MONGO_DB);
+let db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('DB connected');
+});
+
+db.on('error', err => {
+    console.log('DB ERROR : ', err);
+});
+
+//Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+//_method의 query로 들어오는 값으로 HTTP method를 바꿉니다.
+//http://example.com/category/id?_method=delete를 받으면 _method의 값이 delete를 읽어
+//해당 request의 HTTP method를 delete로 바꿉니다.
+
+// DB schema
+let contactSchema = mongoose.Schema({
+    name:{type:String, required:true, unique:true},
+    email:{type:String},
+    phone:{type:String},
+});
+let Contact = mongoose.model('contact', contactSchema);
+
+// Routes
+// Home
+app.get('/', (req, res) => {
+    res.redirect('/contacts');
+})
+// Contacts - Index
+app.get('/contacts', (req, res) => {
+    Contact.find({}, (err, contacts) => {
+        if(err) return res.json(err);
+        res.render('contact/index', {contacts:contacts});
+    });
+});
+// Contacts - New
+app.get('/contacts/new', (req, res) => {
+    res.render('contacts/new');
+});
+// Contacts - create
+app.post('/contacts', (req,res)=> {
+    Contact.create(req.body, (err, contact) => {
+        if(err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
+// Contacts - show
+app.get('/contacts/:id', (req,res) => {
+    // "/contacts/:id"에 get요청이 오는 경우
+    // :id처럼 route에 콜론(:)을 사용하면 해당 위치의 값을 받아 req.params에 넣게 된다.
+    // 예를 들어 "contacts/abcd1234"가 입력되면 "contact/:id" route에서 이를 받아
+    // req.params.id에 "abcd1234"를 넣게 된다.
+    Contact.findOne({_id:req.params.id}, (err, contact) => {
+        //Model.findOne은 DB에서 해당 model의 document를 하나 찾는 함수입니다. 첫번째 parameter로
+        //찾을 조건을 object로 입력하고 data를 찾은 후 콜백 함수를 호출합니다.
+        //Model.find 와 비교해서 Model.find는 조건에 맞는 결과를 모두 찾아 array로 전달하는 데 비해
+        //Model.findOne은 조건에 맞는 결과를 하나 찾아 Object로 전달합니다. (검색 결과가 없다면 null이 전달됩니다.)
+        //위 경우 DB의 contacts collection에서 _id가 req.params.id와 일치하는 data를 찾는 조건
+        if(err) return res.json(err);
+        res.render('contacts/show', {contact:contact});
+        //에러가 없다면 검색 결과를 받아 views/contacts/show.ejs를 render합니다.
+    });
+});
+// Contacts - edit
+app.get('/contacts/:id/edit', (req, res) => {
+    // "contacts/:id/edit" 에 get 요청이 오는 경우 :
+    Contact.findOne({_id:req.params.id}, (err, contact) => {
+        if(err) return res.json(err);
+        res.redirect('/contacts/edit' + req.params.id);
+        //Model.findOne이 다시 사용되었습니다. 검색결과를 받아 views/contacts/edit.ejs를 render합니다.
+    });
+});
+// Contacts - update
+app.put('/contacts/:id', (req, res) =>{
+    // "contacts/:id"에 put 요청이 오는 경우 
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, (err, contact) => {
+        if(err) return res.json(err);
+        res.redirect('/contacts/' + req.params.id);
+    });
+});
+// Contacts - destroy
+app.delete('/contacts/:id', (req, res) => {
+    Contact.deleteOne({_id:req.params.id}, err => {
+        if(err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
+
+//Port setting
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server on! http://localhost:' + port);                                                                                                                ')
 });
